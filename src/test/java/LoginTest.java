cd85735216b3fe8ee128d84c889a47c0a70992e1
hedy <khalfaouimohamed01@gmail.Com>
2026-09-09 00:55:28 +0200

    ajoutproduit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -68,7 +68,7 @@ public class LoginTest {
 
     }
     @Test
-            void ajoutproduit(){
+            void ajoutproduits(){
 
         loginPage.login("standard_user", "secret_sauce");
         productsPage.premierArticle();
